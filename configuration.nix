5bee3232064b5eea7d2df42d457d3bc169b4824b
Axy <gilliardmarthey.axel@gmail.com>
2025-02-03 11:24:26 +0100

    Added wivrn

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,30 @@
 
   services.avahi.enable = true;
 
+  services.wivrn = {
+    enable = true;
+    openFirewall = true;
+    defaultRuntime = true;
+    autoStart = true;
+    config = {
+      enable = true;
+      json = {
+        scale = 1.0;
+        bitrate = 100000000;
+        encoders = [
+          {
+            encoder = "vaapi";
+            codec = "h265";
+            width = 1.0;
+            height = 1.0;
+            offset_x = 0.0;
+            offset_y = 0.0;
+          }
+        ];
+      };
+    };
+  };
+
   users.users.axy = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
