bf1fb44936f4f909cd88763540522b1a4c3da9c6
Axy <gilliardmarthey.axel@gmail.com>
2025-02-05 14:20:31 +0100

    Second typo

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
   boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" ];
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices = 1 video_nr=1 card_label="Virtual Cam" exclusive_caps=1
+    options v4l2loopback devices=1 video_nr=1 card_label="Virtual Cam" exclusive_caps=1
   '';
 
 
