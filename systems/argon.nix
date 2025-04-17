c01f2419bfc204bb347ef50027abd3dd9b5404ac
Axy <gilliardmarthey.axel@gmail.com>
2025-04-17 04:03:15 +0200

    Renamed lithium to argon and made it a desktop that can boot headless instead of just headless

--- systems/argon.nix
+++ systems/argon.nix
@@ -1,7 +1,7 @@
 {
   system = "x86_64-linux";
   features = (import ./features.nix).mkFeatures {
-    headless = true;
+    desktop = true;
   };
   modules = [
     (
--- systems/default.nix
+++ systems/default.nix
@@ -1,5 +1,5 @@
 {
   helium = import ./helium.nix;
   neon = import ./neon.nix;
-  lithium = import ./lithium.nix;
+  argon = import ./argon.nix;
 }
