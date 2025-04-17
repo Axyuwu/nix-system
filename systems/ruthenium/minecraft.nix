a73634a1efb17093a3daeabfd349637b37b7a319
Axy <gilliardmarthey.axel@gmail.com>
2025-04-17 20:33:26 +0200

    typo

--- systems/ruthenium/minecraft.nix
+++ systems/ruthenium/minecraft.nix
@@ -11,9 +11,9 @@
       jdk8
       curl
     ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBeg1XlbH/rtR1uXd5GuWiZuJsmGfUtJHccnODKt6pYi"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfwxBuwDrd/yy/69ZRuj7xdCtanimkCuYVWh2ihM4rM"
+    ];
   };
-  openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBeg1XlbH/rtR1uXd5GuWiZuJsmGfUtJHccnODKt6pYi"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfwxBuwDrd/yy/69ZRuj7xdCtanimkCuYVWh2ihM4rM"
-  ];
 }
