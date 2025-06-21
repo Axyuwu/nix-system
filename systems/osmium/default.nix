e53f5a2155562f1559aa1041d4d51311938e932e
Axy <gilliardmarthey.axel@gmail.com>
2025-06-21 07:56:03 +0000

    Automatic initial commit for system osmium

---
+++ systems/osmium/default.nix
@@ -0,0 +1,24 @@
+{
+  system = "x86_64-linux";
+  headless = true;
+  modules = [
+    {
+      system.stateVersion = "25.05";
+      hardware = {
+        defaultPartitions.enable = true;
+        cpuVendor = "intel"
+        virtualization = "qemu";
+        kvm.enable = false;
+        bootFirmware = "bios"
+      };
+      boot.initrd.availableKernelModules = [
+       "ahci"
+       "sd_mod"
+       "sr_mod"
+       "virtio_pci"
+       "virtio_scsi"
+       "xhci_pci"
+      ];
+    }
+  ];
+}
