7933f3444564d540c50c86684f5967f57c2a531c
Axy <gilliardmarthey.axel@gmail.com>
2025-08-04 20:45:43 +0200

    Removed mail subdomain from mailserver

--- common/mailserver.nix
+++ common/mailserver.nix
@@ -15,17 +15,14 @@ in
       fqdn = "${systemName}.uwuaxy.net";
       domains = [
         "uwuaxy.net"
-        "mail.uwuaxy.net"
       ];
       loginAccounts."axy@uwuaxy.net" = {
         hashedPassword = "$2b$05$cKW9J60CENZUjbpFLXhhkejC/o60KxmItMpO2mslCv5d1Pu8zyRzq";
         aliases = [
           "@uwuaxy.net"
-          "@mail.uwuaxy.net"
         ];
         catchAll = [
           "uwuaxy.net"
-          "mail.uwuaxy.net"
         ];
       };
       certificateScheme = "acme-nginx";
