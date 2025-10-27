7054c3538f595f294beae0a036514a8539ff76a2
Axy <gilliardmarthey.axel@gmail.com>
2025-10-27 21:22:00 +0100

    Changed gitolite umask

--- systems/ruthenium/gitolite.nix
+++ systems/ruthenium/gitolite.nix
@@ -3,5 +3,8 @@
     enable = true;
     adminPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBeg1XlbH/rtR1uXd5GuWiZuJsmGfUtJHccnODKt6pYi";
     user = "git";
+    extraGitoliteRc = ''
+      $RC{UMASK} = 0027;
+    '';
   };
 }
