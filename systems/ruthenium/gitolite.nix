6b9c2281e9903109ae2e3319b16f6665c074289e
Axy <gilliardmarthey.axel@gmail.com>
2025-10-27 23:48:38 +0100

    Added gitweb to ruthenium

--- systems/ruthenium/gitolite.nix
+++ systems/ruthenium/gitolite.nix
@@ -1,12 +1,25 @@
 {
-  services.gitolite = {
-    enable = true;
-    adminPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBeg1XlbH/rtR1uXd5GuWiZuJsmGfUtJHccnODKt6pYi";
-    user = "git";
-    extraGitoliteRc = ''
-      $RC{UMASK} = 0027;
-      $RC{ROLES}{MANAGERS} = 1;
-      $RC{ROLES}{RELEASE_MANAGERS} = 1;
-    '';
+  config,
+  ...
+}:
+{
+  services = {
+    gitolite = {
+      enable = true;
+      adminPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBeg1XlbH/rtR1uXd5GuWiZuJsmGfUtJHccnODKt6pYi";
+      user = "git";
+      extraGitoliteRc = ''
+        $RC{UMASK} = 0027;
+        $RC{ROLES}{MANAGERS} = 1;
+        $RC{ROLES}{RELEASE_MANAGERS} = 1;
+      '';
+    };
+    nginx = {
+      enable = true;
+      gitweb = {
+        enable = true;
+        group = config.services.gitolite.group;
+      };
+    };
   };
 }
