73b2f9d8f924a595680cd2336c41bbd777888b40
Axy <gilliardmarthey.axel@gmail.com>
2026-03-06 00:22:45 +0100

    Attempting to fix untrusted substituter issue

--- common/nix-settings.nix
+++ common/nix-settings.nix
@@ -13,7 +13,7 @@
       "uwuaxy.net/nixcache:Cs1U4hIsAWS1RqbNTKDRM3KbT6MFCp8bfSdX6rfk5/A="
     ];
     trusted-substituters =
-      lib.attrsets.mapAttrsToList (name: _system: "https://${name}.uwuaxy.net/nixcache")
+      lib.attrsets.mapAttrsToList (name: _system: "https://${name}.uwuaxy.net/nixcache/")
         (
           lib.attrsets.filterAttrs (
             _name:
