3e89d453381fa71f5b9d9a19172e2ff537f158e3
Axy <gilliardmarthey.axel@gmail.com>
2026-04-25 16:37:30 +0200

    Updated ssh key

--- systems/ruthenium/gtnh.nix
+++ systems/ruthenium/gtnh.nix
@@ -13,6 +13,7 @@
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBeg1XlbH/rtR1uXd5GuWiZuJsmGfUtJHccnODKt6pYi"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIButG4fIiJcf3JvaWZy0Af09mpo9zuA3K8YgySALVwNN"
     ];
   };
   networking.firewall.allowedTCPPorts = [ 25568 ];
